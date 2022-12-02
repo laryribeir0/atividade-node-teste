26115411ed12d16ce144503992acbf1468b5a33f
laryribeir0 <laryssalary1291@gmail.com>
2022-12-01 23:57:20 -0300

    first commit

---
+++ nodeif/app.js
@@ -0,0 +1,16 @@
+// Requiring module
+const express = require('express');
+
+//Creating express object
+const app =  express();
+
+//Handling GET request
+app.get('/', (req,res) => {
+    res.send( 'A simple Node App is' + 'runing on this server')
+    res.ennd()})
+
+//Port Number
+const PORT = process.env.PORT ||5000;
+
+//Server Setup
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
